f24e2a0b5357c9a5d2a8f367ee2b4d570f9b01ce
Ezra Rieben <ezra.rieben@gmail.com>
2020-03-23 14:11:05 +0100

    Changed table resolver to automated update/install resolver

--- _build/resolvers/resolver.tables.php
+++ _build/resolvers/resolver.tables.php
@@ -1,25 +1,129 @@
 <?php
-$pkgName = 'SamplePackage';
-$pkgNameLower = strtolower($pkgName);
+$packageName = 'SamplePackage';
+$packageNamespace = strtolower($packageName);
+
+set_time_limit(0);
+
+if (!function_exists('updateTableColumns')) {
+    /**
+     * @param modX $modx
+     * @param string $table
+     */
+    function updateTableColumns($modx, $table)
+    {
+        $tableName = $modx->getTableName($table);
+        $tableName = str_replace('`', '', $tableName);
+        $dbname = $modx->getOption('dbname');
+
+        $c = $modx->prepare("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = :dbName AND table_name = :tableName");
+        $c->bindParam(':dbName', $dbname);
+        $c->bindParam(':tableName', $tableName);
+        $c->execute();
+
+        $unusedColumns = $c->fetchAll(PDO::FETCH_COLUMN, 0);
+        $unusedColumns = array_flip($unusedColumns);
+
+        $meta = $modx->getFieldMeta($table);
+        $columns = array_keys($meta);
+
+        $m = $modx->getManager();
+
+        foreach ($columns as $column) {
+            if (isset($unusedColumns[$column])) {
+                $m->alterField($table, $column);
+                $modx->log(modX::LOG_LEVEL_INFO, ' -- altered column: ' . $column);
+                unset($unusedColumns[$column]);
+            } else {
+                $m->addField($table, $column);
+                $modx->log(modX::LOG_LEVEL_INFO, ' -- added column: ' . $column);
+            }
+        }
+
+        foreach ($unusedColumns as $column => $v) {
+            $m->removeField($table, $column);
+            $modx->log(modX::LOG_LEVEL_INFO, ' -- removed column: ' . $column);
+        }
+    }
+}
+
+if (!function_exists('updateTableIndexes')) {
+    /**
+     * @param modX $modx
+     * @param string $table
+     */
+    function updateTableIndexes($modx, $table)
+    {
+        $tableName = $modx->getTableName($table);
+        $tableName = str_replace('`', '', $tableName);
+        $dbname = $modx->getOption('dbname');
+
+        $c = $modx->prepare("SELECT DISTINCT INDEX_NAME FROM INFORMATION_SCHEMA.STATISTICS WHERE table_schema = :dbName AND table_name = :tableName AND INDEX_NAME != 'PRIMARY'");
+        $c->bindParam(':dbName', $dbname);
+        $c->bindParam(':tableName', $tableName);
+        $c->execute();
+
+        $oldIndexes = $c->fetchAll(PDO::FETCH_COLUMN, 0);
+
+        $m = $modx->getManager();
+
+        foreach ($oldIndexes as $oldIndex) {
+            $m->removeIndex($table, $oldIndex);
+            $modx->log(modX::LOG_LEVEL_INFO, ' -- removed index: ' . $oldIndex);
+        }
+
+        $meta = $modx->getIndexMeta($table);
+        $indexes = array_keys($meta);
+
+        foreach ($indexes as $index) {
+            if ($index == 'PRIMARY') continue;
+            $m->addIndex($table, $index);
+            $modx->log(modX::LOG_LEVEL_INFO, ' -- added index: ' . $index);
+        }
+    }
+}
+
+if (!function_exists('alterTable')) {
+    /**
+     * @param modX $modx
+     * @param string $table
+     */
+    function alterTable($modx, $table)
+    {
+        $modx->log(modX::LOG_LEVEL_INFO, ' - Updating columns');
+        updateTableColumns($modx, $table);
+
+        $modx->log(modX::LOG_LEVEL_INFO, ' - Updating indexes');
+        updateTableIndexes($modx, $table);
+    }
+}
 
 if ($object->xpdo) {
-  switch ($options[xPDOTransport::PACKAGE_ACTION]) {
-    case xPDOTransport::ACTION_INSTALL:
-      $modx =& $object->xpdo;
-      $modelPath = $modx->getOption("{$pkgNameLower}.core_path",null,$modx->getOption('core_path')."components/{$pkgNameLower}/").'model/';
-      $modx->addPackage($pkgNameLower,$modelPath);
+    switch ($options[xPDOTransport::PACKAGE_ACTION]) {
+        case xPDOTransport::ACTION_INSTALL:
+        case xPDOTransport::ACTION_UPGRADE:
+            /** @var modX $modx */
+            $modx =& $object->xpdo;
 
-      $manager = $modx->getManager();
-      $modx->setLogLevel(modX::LOG_LEVEL_ERROR);
+            $tables = array(
+                'SamplePackageObject',
+            );
 
-      // adding xpdo objects
-      # $manager->createObjectContainer('SamplePackageObject');
+            $modelPath = $modx->getOption($packageNamespace.'.core_path', null, $modx->getOption('core_path') . 'components/'.$packageNamespace.'/') . 'model/';
+            $modx->addPackage($packageNamespace, $modelPath);
 
-      $modx->setLogLevel(modX::LOG_LEVEL_INFO);
+            // Create non existant tables
+            $manager = $modx->getManager();
+            foreach ($tables as $table) {
+                $manager->createObjectContainer($table);
+            }
 
-      break;
-    case xPDOTransport::ACTION_UPGRADE:
-      break;
-  }
+            // Update existing tables
+            foreach ($tables as $table) {
+                $modx->log(modX::LOG_LEVEL_INFO, 'Altering table: ' . $table);
+                alterTable($modx, $table);
+            }
+
+            break;
+    }
 }
 return true;
